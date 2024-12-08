ff2d1ea23e697ad1be69d2f8ab92107548b3e9ad
LEO PHAM <leopham@192.168.0.243>
2024-12-08 11:10:56 +0700

    add new file

---
+++ draw_square.py
@@ -0,0 +1,26 @@
+#####Turtle Intro######
+
+import turtle as t
+
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.backward(200)
+timmy_the_turtle.right(90)
+timmy_the_turtle.left(180)
+timmy_the_turtle.setheading(0)
+
+
+import turtle as t
+import os
+
+######## Challenge 1 - Draw a Square ############
+square_turtle = t.Turtle()
+square_turtle.shape("turtle")
+square_turtle.color("red")
+for _ in range(4):
+    square_turtle.forward(100)
+    square_turtle.right(90)
+# fdsfdsfs
+dsdsdsdss
